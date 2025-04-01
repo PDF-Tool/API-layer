d5c46f62e4f5c82bcab9e7ae97812293295f390f
jip-erk <erkelensjip@gmail.com>
2025-04-01 14:23:56 +0200

    Frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header class="pb-8">
+    <img alt="Vue logo" src="@/assets/canon.svg" width="190" height="125" />
+  </header>
+  <div class="grid grid-cols-2 gap-4">
+    <div class="w-full">
+      <RouterView />
+    </div>
+    <div class="w-full">
+
+    </div>
+  </div>
+</template>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import { fileSizeFormats, type FileSizeFormat } from '@/lib/utils'
+import { Label } from '@/components/ui/label'
+
+import { ref } from 'vue'
+
+const currentFormat = ref<FileSizeFormat>('MB')
+
+</script>
+
+<template>
+  <main>
+    <div class="grid w-full items-center gap-2">
+      <Label for="size">Size per page</Label>
+      <div class="flex gap-2">
+        <Input type="number" name="size" placeholder="Select image size per page..." />
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Button class="min-w-[70px]"> {{ currentFormat }}
+              <span class="icon-[material-symbols--arrow-drop-down-rounded] text-xl"></span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuRadioGroup v-model="currentFormat">
+              <DropdownMenuRadioItem v-for="format in fileSizeFormats" :key="format" :value="format">
+                {{ format }}
+              </DropdownMenuRadioItem>
+            </DropdownMenuRadioGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </div>
+  </main>
+</template>
