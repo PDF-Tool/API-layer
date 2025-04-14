37e1f0685487c46ce78a7a2d9f9691ec8df52b84
jip-erk <erkelensjip@gmail.com>
2025-04-14 14:27:25 +0200

    frontend api update

--- frontend/src/services/pdf-service.ts
+++ frontend/src/services/pdf-service.ts
@@ -5,7 +5,7 @@ const api = new Api()
 
 export async function makePdf(body: FdfFields) {
   try {
-    const response = await api.post('/response', body)
+    const response = await api.post('/GenerateStart', body)
     return response
   } catch (error) {
     console.error(error)
