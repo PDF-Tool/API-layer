4c127125fe30c9d6bdb2f366de9211a006ebe580
jip-erk <erkelensjip@gmail.com>
2025-04-22 10:36:06 +0200

    frontend tabs

--- frontend/src/components/UsersBar.vue
+++ frontend/src/components/UsersBar.vue
@@ -1,12 +1,21 @@
 <script setup lang="ts">
 import { useWebSocketStore } from '@/stores/webSocketStore'
+import { toSvg } from "jdenticon";
 
 const store = useWebSocketStore()
 
+
+const getUrl = (name: string) => {
+  const svgString = toSvg(name, 100);
+  return 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(svgString);
+}
 </script>
 
 <template>
-  <div class="flex gap-2">
-    <div v-for="user in store.users" class="bg-primary p-1 px-2 rounded-md"> {{ user }}</div>
+  <div class="flex">
+    <div v-for="(user, index) in store.users" :key="user" :style="{ marginLeft: index > 0 ? '-0.75rem' : '0' }"
+      class="bg-primary p-1 rounded-full  w-10 h-10 flex items-center justify-center border-2 border-white relative hover:z-10 transition-transform hover:scale-110">
+      <img :src="getUrl(user)" alt="avatar" class="w-full h-full object-contain">
+    </div>
   </div>
 </template>
