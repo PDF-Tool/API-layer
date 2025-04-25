4557c69f705eb95744ceb25529723a09b033b5e6
ibdrs <ivanbogdanovic677@gmail.com>
2025-04-25 11:33:45 +0200

    Update PDFRandomizer.cs

--- Logic/PDFRandomizer.cs
+++ Logic/PDFRandomizer.cs
@@ -1,40 +1,5 @@
 using System;
 
-class Program
-{
-    static void Main()
-    {
-        Console.WriteLine("Hello, World!");
-
-        RandomizerController controller = new RandomizerController();
-
-        do
-        {
-            Console.WriteLine("Can you give me the following numbers: The number of the minimum data per page");
-            string dataMinString = Console.ReadLine();
-            Console.WriteLine("The number of the maximum data per page");
-            string dataMaxString = Console.ReadLine();
-            Console.WriteLine("The number of the minimum pages");
-            string pageMinString = Console.ReadLine();
-            Console.WriteLine("The number of the maximum pages");
-            string pageMaxString = Console.ReadLine();
-
-            if (controller.SetInput(dataMinString, dataMaxString, pageMinString, pageMaxString))
-            {
-                Console.WriteLine("The random numbers are:");
-                Console.WriteLine("Data per page: " + controller.DataCount + " and the number of pages are: " + controller.PageCount);
-            }
-            else
-            {
-                Console.WriteLine("Invalid input. Please try again.");
-            }
-
-            
-            Console.WriteLine("Do you want to run again? (yes/no)");
-        } while (Console.ReadLine().Trim().ToLower() == "yes");
-    }
-}
-
 class RandomizerModel
 {
     private Random _random;
@@ -46,38 +11,33 @@ class RandomizerModel
 
     public int GenerateRandomNumber(int min, int max)
     {
-        return _random.Next(min, max + 1);//de plus 1 is zodat de maximale waarde ook mee telt 
+        if (min > max)
+        {
+             return _random.Next(max, min + 1); 
+        }
+        return _random.Next(min, max + 1);
     }
 }
 
-class RandomizerController
+public class PDFRandomizer
 {
     private RandomizerModel _model;
 
-    public int DataCount { get; private set; }
-    public int PageCount { get; private set; }
-
-    public RandomizerController()
+    public PDFRandomizer()
     {
         _model = new RandomizerModel();
     }
 
-    public bool SetInput(string dataMinString, string dataMaxString, string pageMinString, string pageMaxString)
+    public (int DataCount, int PageCount) GenerateRandomValues(int dataMin, int dataMax, int pageMin, int pageMax)
     {
-   
-        if (int.TryParse(dataMinString, out int dataMin) &&
-            int.TryParse(dataMaxString, out int dataMax) &&
-            int.TryParse(pageMinString, out int pageMin) &&
-            int.TryParse(pageMaxString, out int pageMax))
+        if (dataMin <= 0 || dataMin >= dataMax || pageMin <= 0 || pageMin >= pageMax)
         {
-
-            if (dataMin > 0 && dataMin < dataMax && pageMin > 0 && pageMin < pageMax)
-            {
-                DataCount = _model.GenerateRandomNumber(dataMin, dataMax);
-                PageCount = _model.GenerateRandomNumber(pageMin, pageMax);
-                return true; 
-            }
+            throw new ArgumentException("Invalid input ranges provided. Ensure min > 0 and min < max for both data and pages.");
         }
-        return false;
+
+        int generatedDataCount = _model.GenerateRandomNumber(dataMin, dataMax);
+        int generatedPageCount = _model.GenerateRandomNumber(pageMin, pageMax);
+
+        return (generatedDataCount, generatedPageCount);
     }
 }
